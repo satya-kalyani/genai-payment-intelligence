34c2e5c975537a38f2f418dc5d9499cc8f79f8b7
satya-kalyani <90590405+satya-kalyani@users.noreply.github.com>
2026-04-21 06:12:14 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,22 @@
+from fastapi import FastAPI
+from app.model import load_model
+from app.rag import query_rag
+
+app = FastAPI()
+
+# Load model once
+model = load_model()
+
+@app.get("/")
+def home():
+    return {"message": "GenAI Payment Intelligence API is running"}
+
 @app.post("/predict")
 def predict(data: list):
     prediction = model.predict([data])
     return {"fraud": int(prediction[0])}
+
+@app.get("/ask")
+def ask(query: str):
+    response = query_rag(query)
+    return {"response": response}
